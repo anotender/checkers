9dff6f04ea06f596f0ab74c0984135ff6c133d8b
Mateusz Nowak <mateusznowak@vp.pl>
2016-05-13 12:02:55 +0200

    Added functions to show a board

--- warcaby.hs
+++ warcaby.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 data Fig = W | B | WD | BD | E deriving Show
 type Board = [[Fig]]
 
@@ -15,11 +17,25 @@ figToChar B = 'b'
 figToChar BD = 'B'
 figToChar E = '.'
 
+fromStr :: String -> [Fig]
 fromStr str = map charToFig str
-toStr l = map figToChar l
+
+toStr :: [Fig] -> String
+toStr l = intersperse ' ' (map figToChar l)
 
 initBoard :: String -> Board
 initBoard str = map fromStr (lines str)
 
-showBoard :: Board -> IO()
-showBoard b = putStr (unlines (map toStr [x | x <- b]))
+boardToStr :: Board -> [String]
+boardToStr b = map toStr [x | x <- b]
+
+addRowNumber num line = (show num) ++ " " ++ line
+addRowNumbers board = zipWith addRowNumber [1..8] board
+
+addColNumbers board = ["  1 2 3 4 5 6 7 8"] ++ board
+
+showBoard :: Board -> String
+showBoard board = unlines (addColNumbers (addRowNumbers (boardToStr board)))
+
+b = initBoard ".b.b.b.b\nb.b.b.b.\n.b.b.b.b\n........\n........\nw.w.w.w.\n.w.w.w.w\nw.w.w.w."
+x = showBoard b
