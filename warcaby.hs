daa6e06adddfaf427f9fd1617cd2d8fd97b443c0
Mateusz Nowak <mateusz.nowak@imac-16.local>
2016-05-18 13:56:54 +0200

    Zajecia

--- warcaby.hs
+++ warcaby.hs
@@ -1,6 +1,7 @@
 import Data.List
 
-data Fig = W | B | WD | BD | E deriving Show
+data Fig = W | B | WD | BD | E deriving (Show,Eq)
+-- data Pos = ()
 type Board = [[Fig]]
 
 charToFig :: Char -> Fig
@@ -47,6 +48,25 @@ replaceWithFig fig (h:t) col = h : replaceWithFig fig t (col - 1)
 setFig :: Fig -> Board -> Int -> Int -> Board
 setFig fig (h:t) 0 col = replaceWithFig fig h col : t
 setFig fig (h:t) row col = h : setFig fig t (row - 1) col
+
+isEmpty :: Board -> Int -> Int -> Bool
+isEmpty board row col = (getFig board row col) == E
+
+isWhite :: Board -> Int -> Int -> Bool
+isWhite board row col = (getFig board row col) == W || (getFig board row col) == WD
+
+isBlack :: Board -> Int -> Int -> Bool
+isBlack board row col = (getFig board row col) == B || (getFig board row col) == BD
+
+isValidPos :: (Int, Int) -> Bool
+isValidPos (row, col) = (row >= 0) && (row <= 7) && (col >= 0) && (col <= 7)
+
+getWhiteFigMoves board row col = filter isValidPos [((row - 1), (col - 1)), ((row - 1), (col + 1))]
+
+getPossibleMoves board row col
+	| isEmpty board row col = []
+	| isWhite board row col = getWhiteFigMoves board row col
+	| isBlack board row col = []
 
 b = initBoard ".b.b.b.b\nb.b.b.b.\n.b.b.b.b\n........\n........\nw.w.w.w.\n.w.w.w.w\nw.w.w.w."
 x = showBoard b
